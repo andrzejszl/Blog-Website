63ad192b913f3432462d3784a23b908702b7953f
andrzejszl <andrzej.sz123@gmail.com>
2022-07-27 00:34:20 +0200

    add about, contact, compose and link them

--- app.js
+++ app.js
@@ -21,17 +21,31 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("home", {
-    homeStartingContent: homeStartingContent,
-    aboutContent: aboutContent,
+    homeStartingContent: homeStartingContent
+  });
+})
+
+app.get("/about", function (req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+})
+
+app.get("/contact", function (req, res) {
+  res.render("contact", {
     contactContent: contactContent
   });
 })
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+})
 
 
-
-
-
+app.post("/compose", function (req, res) {
+  let newInputItem = req.body.newInput;
+  console.log(newInputItem);
+})
 
 
 
